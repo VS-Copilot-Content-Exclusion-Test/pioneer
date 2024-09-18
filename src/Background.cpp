29a95906270ec99f6593323486b76883c68d2e45
Michael Price <miprice@microsoft.com>
2024-09-18 09:29:01 -0600

    Consolidate include directives in Background.cpp
    Removed a newline character between #include "Background.h" and subsequent #include directives in Background.cpp to consolidate them into a single block.

--- src/Background.cpp
+++ src/Background.cpp
@@ -1,7 +1,7 @@
 // Copyright © 2008-2024 Pioneer Developers. See AUTHORS.txt for details
 // Licensed under the terms of the GPL v3. See licenses/GPL-3.txt
 
-#include "Background.h"
+#include "Background.h" 
 
 #include "FileSystem.h"
 #include "FloatComparison.h"
